412a91cc65fd36427a8116e94a8f19a72b9b9961
JosephMarzouk <joseph1marzok@gmail.com>
2024-03-03 11:53:00 +0200

    uploading the app

---
+++ lib/services/newsService.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart';
+import 'package:news_app/models/articlemodel.dart';
+class NewsService {
+  final Dio dio;
+
+  NewsService(this.dio);
+
+  Future<List<ArticleModel>> getTopHeadlines({required String category}) async {
+    try {
+      var response = await dio.get(
+          'https://newsapi.org/v2/top-headlines?country=us&apiKey=c673eba89bb843409cc4db6510555a3a&category=$category');
+
+      Map<String, dynamic> jsonData = response.data;
+
+      List<dynamic> articles = jsonData['articles'];
+
+      List<ArticleModel> articlesList = [];
+
+      for (var article in articles) {
+        ArticleModel articleModel = ArticleModel.fromJson(article);
+        articlesList.add(articleModel);
+      }
+
+      return articlesList;
+    } catch (e) {
+      return [];
+    }
+  }
+}
